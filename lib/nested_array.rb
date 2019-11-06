4c4d233efd5dd4b0feef18d3f6250fe1887b9259
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-06 00:23:07 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,10 +40,43 @@ def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+  ENNET_HOUSE = [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+]
+ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
+  ENNET_HOUSE_WITH_ACADEMY= [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  puts ENNET_HOUSE_WITH_ACADEMY.sort
 end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+    ENNET_HOUSE = [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+]
+ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
+  
+  ENNET_HOUSE_WITH_ACADEMY= [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  ENNET_HOUSE[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
