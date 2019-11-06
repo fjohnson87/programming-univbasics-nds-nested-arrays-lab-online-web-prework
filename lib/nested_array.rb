f568a1da62c50f65b2e48fcc10a4629bd313d939
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-06 00:24:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -82,4 +82,22 @@ end
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
+      ENNET_HOUSE = [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+]
+ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
+  
+  ENNET_HOUSE_WITH_ACADEMY= [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  ENNET_HOUSE[0][1]
+  
 end
